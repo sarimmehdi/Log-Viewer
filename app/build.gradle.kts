3971d52400709ac11073c34dc50b270edf40aef1
Muhammad Sarim Mehdi <36595832+sarimmehdi@users.noreply.github.com>
2025-10-30 23:22:19 +0100

    initial project

---
+++ app/build.gradle.kts
@@ -0,0 +1,63 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.androidApplicationPlugin)
+    alias(libs.plugins.kotlinAndroidPlugin)
+    alias(libs.plugins.kotlinComposePlugin)
+}
+
+android {
+    namespace = "com.sarim.logviewer"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.sarim.logviewer"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidxCoreKtxLibrary)
+    implementation(libs.androidxLifecycleRuntimeKtxLibrary)
+    implementation(libs.androidxActivityComposeLibrary)
+    implementation(platform(libs.androidxComposeBomLibrary))
+    implementation(libs.androidxComposeUiLibrary)
+    implementation(libs.androidxComposeUiGraphicsLibrary)
+    implementation(libs.androidxComposeUiToolingPreviewLibrary)
+    implementation(libs.androidxComposeMaterial3Library)
+    testImplementation(libs.junitLibrary)
+    androidTestImplementation(libs.androidxJunitLibrary)
+    androidTestImplementation(libs.androidxEspressoCoreLibrary)
+    androidTestImplementation(platform(libs.androidxComposeBomLibrary))
+    androidTestImplementation(libs.androidxComposeUiTestJunit4Library)
+    debugImplementation(libs.androidxComposeUiToolingLibrary)
+    debugImplementation(libs.androidxComposeUiTestManifestLibrary)
+}
